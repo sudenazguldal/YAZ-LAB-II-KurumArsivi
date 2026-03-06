70ae94cb9e5c4daafc0d7473f12d5da0c82cef81
Sudenaz Güldal <sude.guldal29@gmail.com>
2026-03-06 08:19:03 +0300

    test: add initial auth routing test for missing token (RED)

---
+++ tests/Dispatcher.Tests/AuthTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Dispatcher.Tests
+{
+    [TestFixture]
+    public class AuthTests
+    {
+        [Test]
+        public void Routing_ShouldReturn401Unauthorized_WhenTokenIsMissing()
+        {
+
+
+            // Act
+            int actualStatusCode = 200;
+
+            // Assert
+            Assert.That(actualStatusCode, Is.EqualTo(401), "ERROR: Dispatcher is allowing requests without a token! Expected 401 Unauthorized.");
+        }
+    }
+}
