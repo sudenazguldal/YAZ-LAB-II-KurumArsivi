995368f0f46072e1acd6c07c6df985546d397509
Sudenaz Güldal <sude.guldal29@gmail.com>
2026-03-11 07:28:09 +0300

    test: add routing test for search service resolution (RED)

--- tests/Dispatcher.Tests/SearchMiddlewareTests.cs
+++ tests/Dispatcher.Tests/SearchMiddlewareTests.cs
@@ -1,27 +1,25 @@
 ﻿using Dispatcher.API.Middlewares;
 using Microsoft.AspNetCore.Http;
-using NUnit.Framework;
-using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Dispatcher.Tests
 {
-    [TestFixture]
-    public class RoutingMiddlewareTests
+    internal class SearchMiddlewareTests
     {
         [Test]
-        public async Task ShouldRoute_ToDocumentService_WhenPathStartsWithDocuments()
+        public async Task ShouldRoute_ToSearchService_WhenPathStartsWithSearch()
         {
             var context = new DefaultHttpContext();
-            context.Request.Path = "/api/documents";
+            context.Request.Path = "/api/search";
 
             var middleware = new RoutingMiddleware(_ => Task.CompletedTask);
 
             await middleware.InvokeAsync(context);
 
             Assert.That(context.Response.Headers.ContainsKey("X-Routed-To"), Is.True);
-            Assert.That(context.Response.Headers["X-Routed-To"].ToString(), Is.EqualTo("document-service"));
+            Assert.That(context.Response.Headers["X-Routed-To"].ToString(), Is.EqualTo("search-service"));
         }
-
-
     }
 }
