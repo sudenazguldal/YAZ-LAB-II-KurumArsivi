439fae63d66f08f4937d3542cab3a1dfb8998741
ceren yıldız <cerenyildiz377@gmail.com>
2026-03-18 23:24:45 +0300

    add sample documents and expose port for testing

--- src/Document.Service/Program.cs
+++ src/Document.Service/Program.cs
@@ -1,4 +1,27 @@
+using Document.Service.Repositories;
+using Document.Service.Services;
+using Document.Service.Settings;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddEnvironmentVariables();
+
+builder.Services.Configure<MongoDbSettings>(options =>
+{
+    options.ConnectionString = Environment.GetEnvironmentVariable("MongoDB__ConnectionString")
+                               ?? "mongodb://localhost:27017";
+    options.DatabaseName = "DocumentDb";
+    options.DocumentsCollection = "documents";
+});
+
+builder.Services.AddHttpClient("SearchService", client =>
+{
+    client.BaseAddress = new Uri(
+        Environment.GetEnvironmentVariable("SearchService__Url")
+        ?? "http://localhost:5002");
+});
+
+builder.Services.AddScoped<IDocumentRepository, DocumentRepository>();
+builder.Services.AddScoped<IDocumentService, DocumentService>();
 
 builder.Services.AddControllers();
 
