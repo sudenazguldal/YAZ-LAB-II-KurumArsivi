e2fcf86d2cd88697f11a4efc9adb31882b3dbe44
ceren yıldız <cerenyildiz377@gmail.com>
2026-03-18 22:00:20 +0300

    add Document model

---
+++ src/Document.Service/Models/Document.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Document.Service.Models;
+
+public class Document
+{
+    [BsonId] // MongoDB'deki _id
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+    //document bilgileri
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
+    public string UploadedBy { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
